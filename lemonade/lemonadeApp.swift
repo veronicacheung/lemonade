04c2cd1fde44d4171ed09ee10c2574e1e88feb77
veronicacheung <veronica.cheung1105@gmai.com>
2023-07-18 13:17:14 -0700

    Initial Commit

---
+++ lemonade/lemonadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemonadeApp.swift
+//  lemonade
+//
+//  Created by Veronica Cheung on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct lemonadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
